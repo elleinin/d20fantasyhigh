25a52e0601199e4d229a1ef85fb44235c7e97d7a
Niña Gabrielle Pascual <ninielle@Ninas-MacBook-Air.local>
2021-07-11 14:26:37 +0800

    js

---
+++ index.js
@@ -0,0 +1,13 @@
+//------------------------------
+//-----QUERYSELECTORS
+//------------------------------
+//LANDING
+//------------------------------
+const land = document.querySelector('.landing');
+
+const fig = document.querySelector('.fig');
+const adaine = document.querySelector('.adaine');
+const gorgug = document.querySelector('.gorgug');
+const fabian = document.querySelector('.fabian');
+const kristen = document.querySelector('.kristen');
+const riz = document.querySelector('.riz');
